a1b58b8fb97f0eb6b3632f1f4c27ce091703c4e6
albcv <albcvad@gmail.com>
2026-01-02 11:05:47 -0500

    commit 5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; 
+use Illuminate\Support\Facades\App; 
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Forzar HTTPS en producción
+        if (App::environment('production') || App::environment('staging')) {
+            URL::forceScheme('https');
+        }
     }
 }
