119b5626bd3d177be0ade04e453996088f4a4637
albcv <albcvad@gmail.com>
2026-01-03 21:56:21 -0500

    new update

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -224,8 +224,7 @@
                             })
                             .catch(error => {
                                 console.error('Error al cargar estadísticas:', error);
-                                // Mostrar mensaje de error al usuario
-                                alert('No se pudieron cargar las estadísticas. Por favor, intente más tarde.');
+                                alert('No se pudieron cargar las estadísticas. Por favor, intente más tarde.', error);
                             });
                     } catch (error) {
                         console.error('Error en la inicialización del script:', error);
